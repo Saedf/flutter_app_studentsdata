dd51b79bbe104d7f314c964da996d66717415b9c
saed.fathi@gmial.com <saed.fathi@gmail.com>
2022-01-11 13:30:42 +0330

    DesignView

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_app_studentsdata/data.dart';
 
@@ -25,6 +27,118 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Android Expert'),
+        ),
+        body: FutureBuilder<List<StudentData>>(
+          future: getStudents(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData && snapshot.data != null) {
+              return ListView.builder(
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (context, index) {
+                    return _Student(
+                      studentData: snapshot.data![index],
+                    );
+                  });
+            } else {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
+        ));
+  }
+}
+
+class _Student extends StatelessWidget {
+  final StudentData studentData;
+  const _Student({Key? key, required this.studentData}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 84,
+      padding: EdgeInsets.all(8),
+      margin: EdgeInsets.fromLTRB(8, 4, 8, 4),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(8),
+          boxShadow: [
+            BoxShadow(blurRadius: 10, color: Colors.black.withOpacity(0.5))
+          ]),
+      child: Padding(
+        padding: const EdgeInsets.only(right: 16),
+        child: Row(
+          children: [
+            Container(
+              width: 64,
+              height: 64,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  color:
+                      Theme.of(context).colorScheme.primary.withOpacity(0.1)),
+              child: Text(
+                studentData.firstName.characters.first,
+                style: TextStyle(
+                    color: Theme.of(context).colorScheme.primary,
+                    fontWeight: FontWeight.w900,
+                    fontSize: 24),
+              ),
+            ),
+            SizedBox(
+              width: 8,
+            ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(studentData.firstName + ' ' + studentData.lastName),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Container(
+                      padding: EdgeInsets.fromLTRB(4, 2, 4, 2),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          color: Colors.grey.shade200),
+                      child: Text(
+                        studentData.course,
+                        style: TextStyle(
+                            fontSize: 10, fontWeight: FontWeight.w700),
+                      ))
+                ],
+              ),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.bar_chart_rounded,
+                  color: Colors.grey.shade400,
+                ),
+                Text(
+                  studentData.score.toString(),
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
